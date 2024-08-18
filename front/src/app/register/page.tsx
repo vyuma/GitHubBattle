57b5e5f22c409cc71bab4ab6ecce81ef9172bb99
clumsy-ug <rutobe740@gmail.com>
2024-08-18 16:46:12 +0900

    front simple implementation

---
+++ front/src/app/register/page.tsx
@@ -0,0 +1,12 @@
+const RegisterPage = () => {
+    return (
+        <div>
+            <h1>新規登録</h1>
+            <input type="text" placeholder="メールアドレス" />
+            <input type="text" name="" id="" placeholder="githubのユーザー名" />
+            <button>登録</button>
+        </div>
+    );
+};
+
+export default RegisterPage;
