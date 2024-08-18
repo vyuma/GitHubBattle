fca43c037d8a082bd3481ccfa8e6868b0ae4cad0
NKMAK <89186723+NKMAK@users.noreply.github.com>
2024-08-18 20:15:36 +0900

    userNameを動的に登録

--- front/src/app/register/page.tsx
+++ front/src/app/register/page.tsx
@@ -5,14 +5,21 @@ import { signup } from '@/service/supabase/auth/signup';
 const RegisterPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [userName, setuserName] = useState('');
 
     const handleSignUp = async (e: FormEvent) => {
         e.preventDefault();
-        await signup(email, password, "test");
+        await signup(email, password, userName);
     };
 
     return (
         <form onSubmit={handleSignUp}>
+            <input
+                type="text"
+                placeholder="名前"
+                value={userName}
+                onChange={(e) => setuserName(e.target.value)}
+            />
             <input
                 type="email"
                 placeholder="Email"
