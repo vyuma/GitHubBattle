ad60b27397b5f00844ab23bb292a5a0747f545d7
NKMAK <89186723+NKMAK@users.noreply.github.com>
2024-08-18 18:57:34 +0900

    新規登録基礎
    参考：https://qiita.com/kitao6/items/01a7ff1e5dea0a922595

--- front/src/app/register/page.tsx
+++ front/src/app/register/page.tsx
@@ -1,11 +1,46 @@
+"use client";
+import { useState, FormEvent } from 'react';
+import { supabase } from "@/utils/supabase/supabase";
+
 const RegisterPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSignUp = async (e: FormEvent) => {
+        e.preventDefault();
+        try {
+            const { error: signUpError } = await supabase.auth.signUp({
+                email: email,
+                password: password,
+                options: {
+                    emailRedirectTo: `${location.origin}/auth/callback`,
+                },
+            });
+            if (signUpError) {
+                throw signUpError;
+            }
+            alert("登録完了");
+        } catch (error) {
+            alert("エラーが発生しました");
+        }
+    };
+
     return (
-        <div>
-            <h1>新規登録</h1>
-            <input type="text" placeholder="メールアドレス" />
-            <input type="text" name="" id="" placeholder="githubのユーザー名" />
-            <button>登録</button>
-        </div>
+        <form onSubmit={handleSignUp}>
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Sign Up</button>
+        </form>
     );
 };
 
