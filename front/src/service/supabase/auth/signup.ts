8388446f9fde5cff46d4543a509916904aa42f35
NKMAK <89186723+NKMAK@users.noreply.github.com>
2024-08-19 11:46:43 +0900

    auth-helpers-nextjsの導入

--- front/src/service/supabase/auth/signup.ts
+++ front/src/service/supabase/auth/signup.ts
@@ -1,25 +1,33 @@
-import { supabase } from "@/service/supabase/supabase";
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { signUpAddUser } from "../updates/signUpAddUser";
 
 export const signup = async (emailValue: string, ps: string, userName: string) => {
-
     try {
-        // アカウント作成
+        const supabase = createClientComponentClient();
         const { data, error } = await supabase.auth.signUp({
             email: emailValue,
             password: ps,
-        })
-        // 特定の条件下
-        if (error) {
-            throw new Error(error.message)
+        });
+
+        if (error) throw new Error(error.message);
+
+        // サインインを実行
+        const { error: signInError } = await supabase.auth.signInWithPassword({
+            email: emailValue,
+            password: ps,
+        });
+
+        if (signInError) throw new Error(signInError.message);
+
+        // ユーザーが認証された後にデータを挿入
+        if (data.user) {
+            await signUpAddUser(data.user.id, userName);
         }
-        alert('アカウントを作成しました。')
-        if (data.user !== null && data.user !== undefined) {
-            // ユーザテーブルにデータを追加
-            signUpAddUser(data.user.id, userName);
-        }
-        console.log(data)
+
+        alert('アカウントを作成しました。');
+        console.log(data);
     } catch (error) {
         console.error('サインアップエラー:', error);
     }
 }
+
