99539bff3779f1422f92209a4031e0f57dd54cc8
NKMAK <89186723+NKMAK@users.noreply.github.com>
2024-08-19 11:51:41 +0900

    ミドルウェアの追加:ログインしてない場合のリダイレクト

---
+++ front/src/middleware.ts
@@ -0,0 +1,33 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+const protectedPaths = ['/home', '/test', '/commynity'];
+
+export async function middleware(req: NextRequest) {
+    try {
+        const res = NextResponse.next();
+        const supabase = createMiddlewareClient({ req, res });
+        const { data: { session } } = await supabase.auth.getSession();
+
+        console.log('Session:', session);
+
+        const path = req.nextUrl.pathname;
+        console.log('Path:', path);
+
+        if (protectedPaths.some(protectedPath => path.startsWith(protectedPath)) && !session) {
+            const redirectUrl = new URL('/login', req.url);
+            redirectUrl.searchParams.set('redirectTo', path);
+            return NextResponse.redirect(redirectUrl);
+        }
+
+        return res;
+    } catch (error) {
+        console.error('Middleware error:', error);
+        return NextResponse.next();
+    }
+}
+
+export const config = {
+    matcher: ['/home/(.*), /test/(.*), /community/(.*)'],
+}
