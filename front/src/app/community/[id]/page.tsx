7bef85344a0164b410daef844bff5d1550f01386
NKMAK <89186723+NKMAK@users.noreply.github.com>
2024-08-20 18:35:13 +0900

    過去のチャット２０件初期表示

--- front/src/app/community/[id]/page.tsx
+++ front/src/app/community/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import Message from "@/components/Message"
 import { receiveChatType } from "@/constants/receiveChatType"
+import { getCommunityChat } from "@/service/supabase/get/getCommunityChat"
 import { fetchRealtimeData } from "@/service/supabase/realtime/fetchRealtime"
 import { addMessageDB } from "@/service/supabase/updates/addMessage"
 import { useEffect, useState } from "react"
@@ -11,7 +12,26 @@ const CommunityChat = ({ params }: { params: { id: string } }) => {
     const [receiveChatData, setReceiveChatData] = useState<receiveChatType[]>([])
 
     useEffect(() => {
-        fetchRealtimeData(setReceiveChatData, params.id)
+        let isMounted = true
+        const cleanupFunction = fetchRealtimeData(setReceiveChatData, params.id)
+
+        const fetchInitialMessages = async () => {
+            try {
+                const initialMessages: receiveChatType[] = await getCommunityChat(params.id)
+                if (isMounted) {
+                    setReceiveChatData(initialMessages)
+                }
+            } catch (error) {
+                console.error('Error fetching initial messages:', error)
+            }
+        }
+
+        fetchInitialMessages()
+
+        return () => {
+            isMounted = false
+            cleanupFunction()
+        }
     }, [params.id])
 
     const handleSendMessage = async () => {
