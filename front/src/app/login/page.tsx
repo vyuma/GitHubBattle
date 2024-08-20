8e49e4713cd5448e87bc986fe574e0a81fee8247
NKMAK <89186723+NKMAK@users.noreply.github.com>
2024-08-20 15:56:43 +0900

    ログイン時にXの名前を保存させる

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -1,40 +1,56 @@
 'use client';
 
-import { useEffect } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useEffect, useState } from 'react'
 import { githubLogin } from '@/service/supabase/auth/githubLogin';
 import { logout } from '@/service/supabase/auth/logout';
+import { useRouter } from 'next/navigation';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { addUser } from '@/service/supabase/updates/addUser';
 
 const LoginPage = () => {
+    const [xName, setXName] = useState<string>('');
     const router = useRouter();
-    const searchParams = useSearchParams();
     const supabase = createClientComponentClient();
 
     useEffect(() => {
-        const code = searchParams.get('code');
-        if (code) {
-            handleAuthCallback();
-        }
-    }, [searchParams])
+        const checkSession = async () => {
+            const { data: { session }, error } = await supabase.auth.getSession();
+            if (session && !error) {
+                const storedXName = localStorage.getItem('xName');
+                const isSuccess = await addUser(session.user, storedXName);
+                if (isSuccess) {
+                    localStorage.removeItem('xName');
+                    router.push('/test');
+                } else {
+                    console.error('Failed to add user');
+                }
+            }
+        };
 
-    const handleAuthCallback = async () => {
-        //分割代入
-        const { data: { session }, error } = await supabase.auth.getSession();
-        if (error) {
-            console.error(error);
-        } else if (session) {
-            router.push('/test');
-        }
+        checkSession();
+    }, []);
+
+    const handleGithubLogin = async () => {
+        localStorage.setItem('xName', xName);
+        await githubLogin();
     }
 
     return (
         <div>
             <h1>ログインページ</h1>
-            <button onClick={githubLogin}>GitHubでログイン</button>
-            <button onClick={logout}>ログアウトテスト</button>
+            <input
+                type="text"
+                value={xName}
+                onChange={(e) => setXName(e.target.value)}
+                placeholder="Xユーザー名（任意）"
+
+            />
+            <button onClick={handleGithubLogin} >
+                GitHubでログイン
+            </button>
+            <button onClick={logout} >ログアウトテスト</button>
         </div>
     )
 }
 
-export default LoginPage
+export default LoginPage;
