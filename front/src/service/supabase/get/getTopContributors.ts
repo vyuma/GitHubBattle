93ae8989fe4d075d9c5be59bc4a6998841c84d59
NKMAK <89186723+NKMAK@users.noreply.github.com>
2024-08-23 19:59:24 +0900

    個人ランキングトップ10取得機能

---
+++ front/src/service/supabase/get/getTopContributors.ts
@@ -0,0 +1,23 @@
+import { userContributionType } from "@/constants/userContributionType";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+
+export const getTopContributors = async (): Promise<userContributionType[]> => {
+    const supabase = createClientComponentClient()
+
+    try {
+        const { data, error } = await supabase
+            .from('user_contribution')
+            .select('*')
+            .order('total_contributions', { ascending: false })
+            .limit(10);
+
+        if (error) {
+            throw error
+        }
+
+        return (data as userContributionType[]);
+    } catch (error) {
+        console.error('Error fetching initial data:', error)
+        return []
+    }
+}
