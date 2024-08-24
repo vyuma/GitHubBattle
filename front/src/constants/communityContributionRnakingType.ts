45976a0467491875d02c9f00f20f714ff481909b
NKMAK <89186723+NKMAK@users.noreply.github.com>
2024-08-24 22:36:34 +0900

    /home コミュニティランキングエラー修正

--- front/src/constants/communityContributionRnakingType.ts
+++ front/src/constants/communityContributionRnakingType.ts
@@ -1,7 +1,7 @@
 export interface communityContributionRnakingType{
     rank: number;
     community_id: string;
-    total_contributions: string;
+    total_contributions: number;
     all_members_committed:boolean;
     community_name: string;
     created_at: string;
